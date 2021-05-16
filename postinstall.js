7c341fab73449e994f8b2f26372d344e09d399d9
steveush <steveush@gmail.com>
2021-05-16 19:14:08 +0200

    Testing prepare script

--- postinstall.js
+++
@@ -1,4 +0,0 @@
-const u = require("./utils");
-const c = require("chalk");
-
-u.log("To get started run '" + c.cyan("npx fp-template setup") + "', modify any values in your '" + c.cyan("package.json") + "' file and then run '" + c.cyan("npx fp-template create") + "'.\n");
---
+++ prepare.js
@@ -0,0 +1,2 @@
+// this is here to force npm to install dependencies when this package is included from a git url or local path
+// @see https://docs.npmjs.com/cli/v7/commands/npm-install
